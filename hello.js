088a467ae2ff6c099363a689052245f4d8d40044
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-05-22 07:41:51 -0500

    Hello, World/messing around

--- hello.js
+++ hello.js
@@ -4,6 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
+console.log ("Hello, World!")
 console.log ("It's a carnival!", '"Nevermore"');
 
 /*  
