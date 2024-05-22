bcafb278ccd278f70ba67f26d3591c39b03b4b65
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-05-22 07:30:02 -0500

    codeplay

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log ("It's a carnival!", '"Nevermore"');
 
 /*  
 Next, PLAY! 
